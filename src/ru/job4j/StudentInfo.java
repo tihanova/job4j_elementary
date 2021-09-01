4d25c28654267aaba07eb034c85f1857c6ec4758
tihanova <a.tihanova@cdek.ru>
2021-09-01 14:51:17 +0300

    3.2. Фиксация изменений на github [#243564]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Tihanova Nastja");
+        System.out.println("01.09.2021");
+    }
+}
