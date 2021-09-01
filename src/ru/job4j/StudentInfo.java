8fb18cf4f9cdb6ab28fc5e0b64a06f1c461adfbf
tihanova <a.tihanova@cdek.ru>
2021-09-01 15:02:03 +0300

    Изменена дата рождения

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Tihanova Nastja");
-        System.out.println("01.09.2021");
+        System.out.println("30.01.1992");
     }
 }
