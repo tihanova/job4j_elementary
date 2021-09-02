af401252c3d689f6d3c1c7d65c311a3953c6bc69
tihanova <a.tihanova@cdek.ru>
2021-09-02 22:31:45 +0300

    3. Арифметические операции. [#185 #197646] Расскоментировала строку

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
         int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
-        //System.out.println(onePlusTwo);
+        System.out.println(onePlusTwo);
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -10,5 +10,11 @@ public class TrgArea {
     public static void main(String[] args) {
         double rsl = TrgArea.area(2, 2, 2);
         System.out.println("area (2, 2, 2) = " + rsl);
+
+        short s = 1500;
+        char c = (char) s;
+        double d = c;
+        System.out.println(d);
+
     }
 }
