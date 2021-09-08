a62cdede29a0f8b97b576445fdd7f72efb6fda09
tihanova <a.tihanova@cdek.ru>
2021-09-08 19:05:52 +0300

    Добаввила сборку мавен в проект

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -3,8 +3,8 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(double p, double k) {
         double  h = p / (2 * (k + 1));
-        double L = h * k;
-        double s = L * h;
+        double l = h * k;
+        double s = l * h;
         return s;
 
     }
