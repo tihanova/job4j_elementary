abbd2ba31ede620cf5a903e7f926965346402494
tihanova <a.tihanova@cdek.ru>
2021-09-13 23:07:51 +0300

    4.6.2. Switch - matcher [#334071]

---
+++ src/test/java/ru/job4j/condition/MultipleSwitchWeekTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class MultipleSwitchWeekTest {
+
+    @Test
+    public void numberOfDay1() {
+        String str = "Понедельник";
+        int result = MultipleSwitchWeek.numberOfDay(str);
+        int expected = 1;
+        Assert.assertEquals(expected, result);
+
+    }
+
+
+}
