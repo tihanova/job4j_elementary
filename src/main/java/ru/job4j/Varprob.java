d0c9d3064d59421e1594b418e7bd4e1018a8d4d7
tihanova <a.tihanova@cdek.ru>
2022-02-04 19:20:45 +0300

    5.2. Создать программу, вычисляющую факториал. [#193]

---
+++ src/main/java/ru/job4j/Varprob.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+public class Varprob {
+
+    public static void main(String[] args) {
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 1;
+        System.out.println(size);
+        long money = 900500;
+        System.out.println(money);
+        float weight = 1.05F;
+        System.out.println(weight);
+        double distance = 100500.99;
+        System.out.println(distance);
+        boolean exists = true;
+        System.out.println(exists);
+        char gender = 'Y';
+        System.out.println(gender);
+        int age = 18;
+        System.out.println(age);
+        float length = 78.0332f;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(baby);
+    }
+}
