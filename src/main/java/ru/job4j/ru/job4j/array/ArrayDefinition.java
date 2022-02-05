34ef2ba070f748abe5a7e21fbdf5ad1c8e89621b
tihanova <a.tihanova@cdek.ru>
2022-02-05 19:16:17 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Nastja Tihanova";
+        names[1] = "Alex Bon";
+        names[2] = "James Bond";
+        names[3] = "Mira Now";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
