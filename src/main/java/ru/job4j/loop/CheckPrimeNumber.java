ad3984414bdf456fdceb4f5dfe9704d5654fc83d
tihanova <a.tihanova@cdek.ru>
2022-02-10 22:01:02 +0300

    Исправления10

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,16 +2,12 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number == 1) {
-            prime = false;
-        }
+        boolean prime = number > 1;
         for (int index = 2; index < number; index++) {
             if (number % index == 0) {
                 prime = false;
                 break;
             }
-            System.out.println(index);
         }
         return prime;
     }
