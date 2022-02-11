21017fd5069d922c99a197c54b590e2678b2a42d
tihanova <a.tihanova@cdek.ru>
2022-02-11 19:17:04 +0300

    Исправления54

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         money = money - price;
-        for (int index = 0; index < coins.length; index++) {
-            while (money >= coins[index]) {
-                money -= coins[index];
-                rsl[size] = coins[index];
+        for (int coin: coins) {
+            while (money >= coin) {
+                money -= coin;
+                rsl[size] = coin;
                 size++;
             }
         }
